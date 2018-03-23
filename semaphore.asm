d44590198a145766a30babafa7c1cccc141eeb43
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-03-23 15:51:54 +0100

    Comments added

--- semaphore.asm
+++ semaphore.asm
@@ -5,41 +5,43 @@ section .text
   extern get_os_time
 
 verhogen:
-  lock add  DWORD [rdi], esi
+  lock add   DWORD [rdi], esi
   ret
 
 revert:
-  lock add  DWORD [rdi], esi
+  lock add   DWORD [rdi], esi
 
 proberen:
   cmp   DWORD [rdi], esi
   jl    proberen
 
+  ;clear edx register and store neg esi in it
   xor   edx, edx
   sub   edx, esi
   lock xadd   DWORD [rdi], edx
+  ;now edx holds old semaphore value (before xadd)
 
+  ;compare old semaphore value with value subtracted (and revert subtraction if needed)
   cmp   edx, esi
   jl    revert
   ret
 
 proberen_time:
+  ;push 8 bytes so stack is 16 bytes aligned when we call another function
+  sub   rsp, 0x8
   push  rdi
   push  rsi
 
-  sub   rsp, 0x8
   call  get_os_time
-  add   rsp, 0x8
 
   pop   rsi
   pop   rdi
-  mov   r10, rax
+  mov   r10, rax   ;store entry time in r10
 
-  sub   rsp, 0x8
   call  proberen
 
   call  get_os_time
-  add   rsp, 0x8
+  add   rsp, 0x8   ;pop 8 bytes added earlier
 
-  sub   rax, r10
+  sub   rax, r10   ;leave time - entry time
   ret
